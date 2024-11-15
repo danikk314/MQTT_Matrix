a4c57423df87420558af3f3d7fd559f155bf8165
danikk314 <145961172+danikk314@users.noreply.github.com>
2024-11-15 09:08:38 +0100

    c6

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let colr;
+
 
 const container = document.getElementById('container');
 
@@ -6,5 +8,11 @@ for (let i = 1; i <= 64; i++) {
   const pixel = document.createElement("div");
   pixel.id = `px${i}`;
   pixel.classList.add("pixel");
+  pixel.setAttribute("onclick", "setColor(this)");
   container.appendChild(pixel);
+}
+
+function setColor(element) {
+  colr = document.getElementById('color').value
+  element.style.backgroundColor = colr;
 }
