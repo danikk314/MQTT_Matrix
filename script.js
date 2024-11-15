8523777c164e84dd07e164a389191c85031cd0f5
danikk314 <145961172+danikk314@users.noreply.github.com>
2024-11-15 10:16:29 +0100

    c9

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 let colr;
-
+let payload = "";
 
 const container = document.getElementById('container');
 
+const options = {
+  username: 'Daniel_Braun',  
+  password: '31415926535Pi',
+  clientId: 'mqtt_client_' + Math.random().toString(16).substr(2, 8), 
+  clean: true               
+};
+
+const client = mqtt.connect('wss://xfa25392.ala.eu-central-1.emqxsl.com:8084/mqtt', options);
+
+client.on('connect', () => {
+  console.log('Mit dem MQTT-Broker verbunden');
+  document.getElementById('connection').src = "wifi.png";
+});
+
+client.subscribe('Mpx', (err) => {
+  if (err) {
+     console.error('Abonnement-Fehler:', err);
+  } else {
+     console.log('Erfolgreich abonniert auf Topic: msg1');
+  }
+});
+
+client.on('error', (error) => {
+  console.error('Verbindungsfehler:', error);
+  document.getElementById('connection').src = "no-wifi.png";
+});
+
+client.on('disconnect', () => {
+  console.log('Vom Broker getrennt');
+  document.getElementById('connection').src = "no-wifi.png";
+});
 
 for (let i = 1; i <= 64; i++) {
   const pixel = document.createElement("div");
@@ -15,4 +46,8 @@ for (let i = 1; i <= 64; i++) {
 function setColor(element) {
   colr = document.getElementById('color').value
   element.style.backgroundColor = colr;
+}
+function sendMessage() {
+  client.publish('Mpx', payload); 
+  console.log('Nachricht gesendet:', payload);
 }
